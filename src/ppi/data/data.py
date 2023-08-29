bd0a821e219c97926b88042e5bebcaefdfc626b5
Johannes Melsbach <github@melsbach.org>
2023-08-29 12:30:02 +0000

    fix: add tokenization for protbert model

--- src/ppi/data/data.py
+++ src/ppi/data/data.py
@@ -1,10 +1,13 @@
 from torch.utils.data import Dataset
+from transformers import AutoTokenizer
+import re
 
 
 class PPIDataset(Dataset):
-    def __init__(self, data, tokenizer, max_length=512, return_labels=True):
+    def __init__(self, data, base_model, max_length=512, return_labels=True):
         self.data = data
-        self.tokenizer = tokenizer
+        self.base_model = base_model
+        self.tokenizer = AutoTokenizer.from_pretrained(base_model) 
         self.max_length = max_length
         self.return_labels = return_labels
 
@@ -15,6 +18,16 @@ class PPIDataset(Dataset):
         sequence_a = self.data["sequenceA"].iloc[idx]
         sequence_b = self.data["sequenceB"].iloc[idx]
         label = self.data["class"].iloc[idx]
+
+        if "yarongef".lower() in self.base_model.lower() or "rostlab" in self.base_model.lower():
+            # replace all rare/ambiguous amino acids by X and introduce white-space between all amino acids
+            # See https://huggingface.co/Rostlab/prot_bert#preprocessing
+            sequence_a = ' '.join(sequence_a)
+            sequence_b = ' '.join(sequence_b)
+            sequence_a = re.sub(r"[UZOB]", "X", sequence_a)
+            sequence_b = re.sub(r"[UZOB]", "X", sequence_b)
+
+
 
         inputs_a = self.tokenizer(
             sequence_a,
