a005cb15c7ec09cf7b9401892b3e60d41ec4623f
Johannes Melsbach <github@melsbach.org>
2023-08-29 12:30:30 +0000

    fix: change batch size

--- src/ppi/debug.py
+++ src/ppi/debug.py
@@ -4,7 +4,7 @@ from ppi.models.contrastive_ppi import ContrastivePPI
 from lightning.pytorch import Trainer
 import lightning.pytorch as pl
 
-datamodule = ContrastiveDataModule(data_path="data/madan", batch_size=4)
+datamodule = ContrastiveDataModule(data_path="data/madan", batch_size=256)
 model = ContrastivePPI(base_model="Rostlab/prot_bert")
 trainer = Trainer()
 trainer.fit(model, datamodule)
