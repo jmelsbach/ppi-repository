37ce68d100387b21d63b64f1d46b6764bbfcfd66
Johannes Melsbach <github@melsbach.org>
2023-08-30 11:58:09 +0000

    fix: change Trainer parameters

--- src/ppi/debug.py
+++ src/ppi/debug.py
@@ -4,7 +4,7 @@ from ppi.models.contrastive_ppi import ContrastivePPI
 from lightning.pytorch import Trainer
 import lightning.pytorch as pl
 
-datamodule = ContrastiveDataModule(data_path="data/madan", batch_size=256)
+datamodule = ContrastiveDataModule(data_path="data/madan", batch_size=4)
 model = ContrastivePPI(base_model="Rostlab/prot_bert")
-trainer = Trainer()
+trainer = Trainer(precision="16-mixed")
 trainer.fit(model, datamodule)
